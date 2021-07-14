cbf62ce48ac7349cf17ed923a3e0b3fc18462448
nkpoid <nkpoidjp@gmail.com>
2021-07-15 05:18:36 +0900

    実装を追加

--- mmf.go
+++ mmf.go
@@ -1,23 +1,73 @@
 package mmf
 
 import (
+	"errors"
+
 	"github.com/google/uuid"
-	"open-match.dev/open-match/pkg/pb"
+	om "open-match.dev/open-match/pkg/pb"
 )
 
-const MatchFunctionName = "dualstack-demo-match-function"
+const (
+	MatchFunctionName = "dualstack-demo-match-function"
+	v4Tag             = "v4"
+	v6Tag             = "v6"
+)
 
-func makeMatch(tickets []*pb.Ticket, profile *pb.MatchProfile) (*pb.Match, error) {
-	return &pb.Match{
+var FailedMatchMakeErr = errors.New("failed to create match")
+
+func extractTicketsWithTag(tickets []*om.Ticket, tag string) []*om.Ticket {
+	contains := func(s []string, e string) bool {
+		for _, v := range s {
+			if e == v {
+				return true
+			}
+		}
+		return false
+	}
+
+	var ret []*om.Ticket
+
+	for _, ticket := range tickets {
+		ticket := ticket
+		if contains(ticket.SearchFields.GetTags(), tag) {
+			ret = append(ret, ticket)
+		}
+	}
+
+	return ret
+}
+
+// makeMatch は，Ticketの配列を受け取り，1v1のマッチを最大1つ作成します．
+// マッチメイキングに失敗した場合，FailedMatchMakeErrを返却します．
+func makeMatch(tickets []*om.Ticket, profile *om.MatchProfile) (*om.Match, error) {
+	match := &om.Match{
 		MatchId:       uuid.NewString(),
 		MatchProfile:  profile.Name,
 		MatchFunction: MatchFunctionName,
-		Tickets:       []*pb.Ticket{},
-	}, nil
+	}
+
+	// 最初に，v6ユーザーの抽出を試みる
+	v6Tickets := extractTicketsWithTag(tickets, v6Tag)
+	if len(v6Tickets) >= 2 {
+		match.Tickets = v6Tickets[:2]
+		return match, nil
+	}
+
+
+	// 次に，v4ユーザー(含v6 dualstack)の抽出を試みます．
+	v4Tickets := extractTicketsWithTag(tickets, v4Tag)
+	if len(v4Tickets) >= 2 {
+		match.Tickets = v4Tickets[:2]
+		return match, nil
+	}
+
+	return nil, FailedMatchMakeErr
 }
 
-func MakeMatches(ticketPools map[string][]*pb.Ticket, profile *pb.MatchProfile) ([]*pb.Match, error) {
-	var matches []*pb.Match
+// MakeMatches は，チケットプールを受け取り，メイキングしたマッチのリストを返却します．
+// 1件もマッチング成功しなかった場合，FailedMatchMakeErrを返却します．
+func MakeMatches(ticketPools map[string][]*om.Ticket, profile *om.MatchProfile) ([]*om.Match, error) {
+	var matches []*om.Match
 
 	for _, tickets := range ticketPools {
 		// TODO: poolを可能な限り使い切るようにする
@@ -26,6 +76,10 @@ func MakeMatches(ticketPools map[string][]*pb.Ticket, profile *pb.MatchProfile) 
 			continue
 		}
 		matches = append(matches, match)
+	}
+
+	if len(matches) == 0 {
+		return nil, FailedMatchMakeErr
 	}
 
 	return matches, nil
